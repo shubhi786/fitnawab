5996d864c96523fabb08f33ee9dfdec9d3febda8
mrrobot <rebilliuzleo007@gmail.com>
2020-07-06 15:02:31 +0530

    new file uploaded

---
+++ js/owl.js
@@ -0,0 +1,36 @@
+$('.img').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('#reviews').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:true,
+    dots:false,
+    nav:true,
+    navText:['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
