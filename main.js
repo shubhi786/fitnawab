0e37e2d08863e75d7963aac66edfcc732c67527d
mrrobot <rebilliuzleo007@gmail.com>
2020-07-08 12:43:16 +0530

    responsive changes and font awesome cdn updates

---
+++ main.js
@@ -0,0 +1,21 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  //Toggle Nav
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+  //Animate Links
+  navLinks.foreach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation =
+        "navLinkFade 0.5s ease forwards ${index /7 + 1.5}s";
+    }
+    console.log(index / 7);
+  });
+};
+navSlide();
